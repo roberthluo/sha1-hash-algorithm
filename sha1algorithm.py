f7f314cbac22589df23fc007f2b791da1ad9638f
Robert <robert.otherone@gmail.com>
2017-03-25 23:55:57 -0400

    Created sha1 object, and set minim byte length

--- sha1algorithm.py
+++ sha1algorithm.py
@@ -6,6 +6,8 @@ import io
 import string
 
 
+
+
 def split(inputStr):
     print "This is count"
     textArray = list(inputStr)
@@ -16,3 +18,13 @@ def split(inputStr):
 def convertToASCII(textArray):
     for t in textArray:
         print ord(t)
+
+class Sha1Hash(object):
+    _message_byte_length = 0
+
+    def __init__(self, message_byte_length):
+        self._message_byte_length = message_byte_length
+
+    def _left_rotate(n, b):
+        #Left rotate a 32-bit integer n by b bits.
+        return ((n << b) | (n >> (32 - b))) & 0xffffffff
--- testalgorithm.py
+++ testalgorithm.py
@@ -11,3 +11,4 @@ import sha1algorithm
 textArray = sha1algorithm.split("A Test")
 
 sha1algorithm.convertToASCII(textArray)
+sha1object = sha1algorithm.Sha1Hash(32)
