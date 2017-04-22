4b01c1fbfdd21f0c0c9dd0e3281c979fdb787cbe
Robert <robert.otherone@gmail.com>
2017-04-22 19:41:20 -0400

    Added message padding

--- sha1algorithm.py
+++ sha1algorithm.py
@@ -35,8 +35,10 @@ def convert_to_ASCII(text_array):
     return full_bin_str
 
 #part 6
+#explaination: https://crypto.stackexchange.com/questions/41431/what-does-congruent-to-448-modulo-512-mean-for-padding-in-md5-hash-function?newreg=8172741ff41346fba02eabda35bbc27b
 def append_zeros(full_bin_str):
         #Add zeros to end until length is congrunent to 448 mod 512
+        orig_length = len(full_bin_str) - 1
         length = len(full_bin_str)
         while(length%512 != 0):
             length = len(full_bin_str)
@@ -44,9 +46,24 @@ def append_zeros(full_bin_str):
             full_bin_str = full_bin_str + '0'
 
         print full_bin_str
-        #append zeros
 
+        #append zeros
+        orig_length_bin = bin(orig_length)
+        orig_length_str = str(orig_length_bin[2:])
+        print ("orig_length_bin", orig_length, orig_length_str)
+
+        #need to add 64 bits
+        mess_padding = orig_length_str
+        while(len(mess_padding) < 64):
+            mess_padding = '0' + mess_padding
+        print ('length', len(mess_padding), mess_padding)
+
+        total_message = full_bin_str + mess_padding
+        print total_message
         #Append original message length
+
+#chunk message
+
 
 class Sha1Hash(object):
     _message_byte_length = 0
