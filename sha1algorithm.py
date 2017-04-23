d13bed85f34f1a3fa896676bd6a70dc0cabb13c6
Robert <robert.otherone@gmail.com>
2017-04-23 15:48:10 -0400

    Extended message into 80 words

--- sha1algorithm.py
+++ sha1algorithm.py
@@ -60,10 +60,79 @@ def append_zeros(full_bin_str):
 
         total_message = full_bin_str + mess_padding
         print total_message
+        return total_message
         #Append original message length
 
 #chunk message
+#split each chunk to sixteen 32-bit words, return a list
+def split_chunk(total_message, length = 32):
+    #return (total_message[0+i:length+i] for i in range(0, len(total_message), length))
+    chunk_array = []
+    counter = 0
+    temp_chunk = ''
+    print length
+    for bits in total_message:
+        temp_chunk = temp_chunk + bits
+        if counter == length:
+            counter = 0
+            chunk_array.append(temp_chunk)
+            temp_chunk = ''
+        counter = counter+ 1
 
+    #print chunk_array
+    return chunk_array
+
+def extend_to_eight_words(chunk_array):
+    #starts off with 0, 2, 8, 13
+    #hard coding to loop 16 times
+    temp_chunk_array = chunk_array
+    fourth = 13
+    third = 8
+    second = 2
+    first = 0
+    out = 17
+
+    list_counter = range(80)
+    for i in list_counter:
+        list_counter[i] = False
+
+
+    for i in range(79 - 16):
+        print 'value of i',i
+        list_counter[fourth] = True
+        list_counter[third] = True
+        list_counter[second] = True
+        list_counter[first] = True
+
+        print("index", fourth, third, second, first)
+        fourth_word = int(temp_chunk_array[fourth])
+        third_word = int(temp_chunk_array[third])
+        second_word = int(temp_chunk_array[second])
+        first_word = int(temp_chunk_array[first])
+        print "Words", fourth_word, third_word, second_word, first_word
+        temp_word = fourth_word^third_word
+        temp_word = temp_word^second_word
+        temp_word = temp_word^first_word
+
+        temp_chunk_array.append(temp_word)
+
+        first = first + 1
+        second = second + 1
+        third = third + 1
+        fourth = fourth + 1
+        out = out + 1
+
+        print "Words len", len(temp_chunk_array)
+        #while list_counter[first] :
+        #    first = first + 1
+        #while list_counter[second] or second == first:
+        #    second = second + 1
+        #while list_counter[third] or third == second or third == first:
+        #    third = third + 1
+        #while list_counter[fourth] or fourth == third or fourth == second or fourth == first:
+        #    fourth = fourth + 1
+        #while list_counter[out] or out == fourth or out == third or out == second or out == first:
+        #    out = out + 1
 
 class Sha1Hash(object):
     _message_byte_length = 0
--- testalgorithm.py
+++ testalgorithm.py
@@ -12,4 +12,9 @@ textArray = sha1algorithm.split("A Test")
 
 full_bin_str = sha1algorithm.convert_to_ASCII(textArray)
 
-sha1algorithm.append_zeros(full_bin_str)
+total_message = sha1algorithm.append_zeros(full_bin_str)
+
+chunked_message = sha1algorithm.split_chunk(total_message)
+
+eight_words = sha1algorithm.extend_to_eight_words(chunked_message)
+#print ('chunked_message', chunked_message)
